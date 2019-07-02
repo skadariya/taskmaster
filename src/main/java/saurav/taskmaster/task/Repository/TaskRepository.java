bdaf19b7b98066f5316d6b0fbe27f7d5fb16a4c3
sauravka <sauravka@amazon.com>
2019-07-02 13:09:25 -0700

    need to fix deployment error

---
+++ src/main/java/saurav/taskmaster/task/Repository/TaskRepository.java
@@ -0,0 +1,13 @@
+package saurav.taskmaster.task.Repository;
+
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+import saurav.taskmaster.task.Model.Task;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableScan
+public interface TaskRepository extends CrudRepository<Task, UUID> {
+    Optional<Task> findById(UUID id);
+}
