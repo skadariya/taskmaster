7b244963942061330599d4e3ecfd24677729eb64
sauravka <sauravka@amazon.com>
2019-07-22 11:22:56 -0700

    SMS and SES working

--- src/main/java/saurav/taskmaster/task/Controller/TasksController.java
+++ src/main/java/saurav/taskmaster/task/Controller/TasksController.java
@@ -1,22 +1,26 @@
 package saurav.taskmaster.task.Controller;
 
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.MessageAttributeValue;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
+import saurav.taskmaster.task.Config.AwsSESSample;
 import saurav.taskmaster.task.Model.Task;
 import saurav.taskmaster.task.Repository.S3Client;
 import saurav.taskmaster.task.Repository.TaskRepository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @RestController
 public class TasksController {
 
     private S3Client s3Client;
+    private AwsSESSample sesEmail;
 
     @Autowired
     TaskRepository taskRepository;
@@ -38,6 +42,7 @@ public class TasksController {
             task.setStatus("Available");
         }else {
             task.setStatus("Assigned");
+            sentSMS(task.getTitle());
         }
         taskRepository.save(task);
         List<Task> allTask = (List) taskRepository.findAll();
@@ -53,12 +58,19 @@ public class TasksController {
         Task task = taskRepository.findById(id).get();
         if(task.getStatus().equals("Available")){
             task.setStatus("Assigned");
+            sentSMS(task.getTitle());
         }
         else if(task.getStatus().equals("Assigned")){
             task.setStatus("Accepted");
         }
         else if(task.getStatus().equals("Accepted")){
             task.setStatus("Finished");
+            /*
+            Lab : SQS with Lambda
+            Sent email when a task is complete
+             */
+            sesEmail.sendEmail(task.getTitle());
+
         }
         taskRepository.save(task);
         List<Task> allTask = (List)taskRepository.findAll();
@@ -104,5 +116,31 @@ public class TasksController {
     public Task addImages(@PathVariable UUID id){
         Task selectedTask = taskRepository.findById(id).get();
         return selectedTask;
+    }
+
+    /*
+    USING SMS
+    Reference: https://docs.aws.amazon.com/sns/latest/dg/sms_publish-to-phone.html
+     */
+    private void sentSMS(String taskName){
+
+            AmazonSNSClient snsClient = new AmazonSNSClient();
+            String message = taskName + " has completed.";
+            String phoneNumber = "+15809196943";
+            Map<String, MessageAttributeValue> smsAttributes =
+                    new HashMap<String, MessageAttributeValue>();
+            //<set SMS attributes>
+            PublishResult result = snsClient.publish(new PublishRequest()
+                    .withMessage(message)
+                    .withPhoneNumber(phoneNumber)
+                    .withMessageAttributes(smsAttributes));
+            System.out.println(result); // Prints the message ID.
+
+    }
+    /*
+    Sent Email
+     */
+    private void sentEmail(String taskName){
+
     }
 }
